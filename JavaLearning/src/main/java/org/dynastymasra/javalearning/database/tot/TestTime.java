f4fa979f2960b56734805a076490eed2443f8efe
dynastymasra <dynastymasra@gmail.com>
2014-06-02 21:50:48 +0700

    edit current time in milis

---
+++ JavaLearning/src/main/java/org/dynastymasra/javalearning/database/tot/TestTime.java
@@ -0,0 +1,18 @@
+package org.dynastymasra.javalearning.database.tot;
+
+/**
+ * Author   : Dynastymasra
+ * Name     : Dimas Ragil T
+ * Email    : dynastymasra@gmail.com
+ * LinkedIn : http://www.linkedin.com/in/dynastymasra
+ * Blogspot : dynastymasra.wordpress.com | dynastymasra.blogspot.com
+ */
+public class TestTime {
+    public static void main(String[] args) {
+        Long time = System.currentTimeMillis();
+        System.out.println("Time : " + time);
+        System.out.println("Long MAX : " + Long.MAX_VALUE);
+        System.out.println("Double MAX : " + Double.MAX_VALUE);
+        System.out.println("Float MAX : " + Float.MAX_VALUE);
+    }
+}
