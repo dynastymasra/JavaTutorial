7e0e60a3daa37d1a4a90991cd6b1d6c22cddb2ef
dynastymasra <dynastymasra@gmail.com>
2014-06-02 21:02:13 +0700

    add database connection

--- JavaLearning/src/main/java/org/dynastymasra/javalearning/database/tot/Main.java
+++ JavaLearning/src/main/java/org/dynastymasra/javalearning/database/tot/Main.java
@@ -1,7 +1,6 @@
 package org.dynastymasra.javalearning.database.tot;
 
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 /**
  * Author   : Dynastymasra
@@ -11,17 +10,30 @@ import java.sql.SQLException;
  * Blogspot : dynastymasra.wordpress.com | dynastymasra.blogspot.com
  */
 public class Main {
-    private final static String URL = "jdbc:mysql://localhost:3306/tot";
+    private final static String URL = "jdbc:mysql://localhost:3306/travel?zeroDateTimeBehavior=convertToNull";
     private final static String USER = "root";
     private final static String PASSWORD = "";
+    private static Connection connection;
 
     public static void main(String[] args) {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            DriverManager.getConnection(URL, USER, PASSWORD);
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
             System.out.println("Database Connection Success!");
+
+            String sql = "SELECT * FROM person";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()) {
+                System.out.println("--------------------------------------");
+                System.out.println("Id : " + resultSet.getInt("personid"));
+                System.out.println("Name : " + resultSet.getString("name"));
+                System.out.println("Job Title : " + resultSet.getString("jobtitle"));
+                System.out.println("Frequent Flyer : " + resultSet.getInt("frequentflyer"));
+                System.out.println("Last Update : " + resultSet.getDate("lastupdated"));
+            }
         } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
     }
 }
