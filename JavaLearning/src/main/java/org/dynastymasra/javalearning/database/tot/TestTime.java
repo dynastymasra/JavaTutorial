79e6eafe7f97da499baedd3388a5e2e95f5ccd36
dynastymasra <dynastymasra@gmail.com>
2014-06-03 12:00:07 +0700

    ++++

--- JavaLearning/src/main/java/org/dynastymasra/javalearning/database/tot/TestTime.java
+++ JavaLearning/src/main/java/org/dynastymasra/javalearning/database/tot/TestTime.java
@@ -1,5 +1,8 @@
 package org.dynastymasra.javalearning.database.tot;
 
+import java.sql.Date;
+import java.util.Calendar;
+
 /**
  * Author   : Dynastymasra
  * Name     : Dimas Ragil T
@@ -9,7 +12,12 @@ package org.dynastymasra.javalearning.database.tot;
  */
 public class TestTime {
     public static void main(String[] args) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2014, Calendar.DECEMBER, 12);
+        Long timePast = calendar.getTimeInMillis();
         Long time = System.currentTimeMillis();
+        Long dif = time - timePast;
+
         System.out.println("Time : " + time);
         System.out.println("Long MAX : " + Long.MAX_VALUE);
         System.out.println("Double MAX : " + Double.MAX_VALUE);
